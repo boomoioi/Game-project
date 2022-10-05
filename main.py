ef8e49dd638854ccd92fed9eae1abae4d9559d8c
Nanthakorn <64312642+boomoioi@users.noreply.github.com>
2022-10-06 00:53:26 +0700

    yeahhhhhhhhhhhhhhhhhhhhhhhhh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame, sys
 from settings import *
+from level import Level
 
 class Game:
     def __init__(self):
@@ -10,6 +11,8 @@ class Game:
         pygame.display.set_caption('Vizkus')
         self.clock = pygame.time.Clock()
 
+        self.level = Level()
+
     def run(self):
         while(True):
             for event in pygame.event.get():
@@ -17,9 +20,10 @@ class Game:
                     pygame.quit()
                     sys.exit
 
-                self.screen.fill('black')
-                pygame.display.update()
-                self.clock.tick(FPS)
+            self.screen.fill('black')
+            self.level.run()
+            pygame.display.update()
+            self.clock.tick(FPS)
 
 if __name__ == '__main__':
     game = Game()
